873b9ac1066f91d330ace89aa6247ed7e57ac614
qinghe <iwangqinghe@gmail.com>
2020-02-07 18:46:44 +0800

    wrapping up

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './icon.png'
 
 function component() {
     let element = document.createElement('div');
   
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack', '你好'], ' ');
-    element.classList.add('hello')
-
-    const myIcon = new Image()
-    myIcon.src = Icon
-
-    element.appendChild(myIcon)
 
     return element;
   }
