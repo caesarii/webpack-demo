8a367ae43ad8131689af910f3a28958693d96953
qinghe <iwangqinghe@gmail.com>
2020-02-07 19:01:41 +0800

    html webapck plugin and clean webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash'
+import printMe from './print.js'
 
 function component() {
     let element = document.createElement('div');
-  
+    const btn = document.createElement('button')
+    
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack', '你好'], ' ');
+
+    btn.innerHTML = 'click me'
+    btn.onclick = printMe
+    
+    element.appendChild(btn)
 
     return element;
   }
