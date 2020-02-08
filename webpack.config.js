075822253df9a474bd0789149945badcc3586334
qinghe <iwangqinghe@gmail.com>
2020-02-08 11:23:50 +0800

    source map

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,13 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js',
   },
+  // 启用 source map sources 中的文件会有所变化
+  devtool: 'inline-source-map',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
