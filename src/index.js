3360dbfb400b7f9e5d1a731b5032a3e618e6f548
qinghe <iwangqinghe@gmail.com>
2020-02-12 21:57:08 +0800

    按需将依赖中的个别模块设置为全局变量

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function component() {
     let element = document.createElement('div');
   
     // 全局依赖 lodash
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = join(['Hello', 'webpack'], ' ');
   
     return element;
   }
