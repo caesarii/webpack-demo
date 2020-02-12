cd66193400e31db6e3e1271c5b395194bb781f7e
qinghe <iwangqinghe@gmail.com>
2020-02-12 22:16:22 +0800

    imports loader

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ function component() {
   
     // 全局依赖 lodash
     element.innerHTML = join(['Hello', 'webpack'], ' ');
+
+    // 假设我们处于 `window` 上下文
+   this.alert('Hmmm, this probably isn\'t a great idea...')
   
     return element;
   }
