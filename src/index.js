095aab50be6ce86bab2e0ebc86a0d9c948740949
qinghe <iwangqinghe@gmail.com>
2020-02-12 19:29:46 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import printMe from './print.js'
+import _ from 'lodash';
 
-function getComponent() {
-    return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-      var element = document.createElement('div');
+function component() {
+  var element = document.createElement('div');
+  var button = document.createElement('button');
+  var br = document.createElement('br');
 
-        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  button.innerHTML = 'Click me and look at the console!';
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
 
-        return element;
+  // Note that because a network request is involved, some ndication
+  // of loading would need to be shown in a production-level site/app
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default
+    print();
+  });
 
-    }).catch(error => 'An error occurred while loading the component');
+    return element;
 }
 
-getComponent().then(component => {
-   document.body.appendChild(component);
-})
+document.body.appendChild(component());
   
--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,14 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
 
 module.exports = {
-  mode: 'development',
   entry: {
     app: './src/index.js',
 
-  },
-  // 启用 source map sources 中的文件会有所变化
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
   },
   output: {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
   },
   plugins: [
       new CleanWebpackPlugin(),
